215a0e6b0fc2b00cab8a9f6e71cb939de4efd4e6
Liz <eliz.com>
2020-03-13 16:56:31 +0300

    14.1

---
+++ LabsC-/Delegate14.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabsC_
+{
+    delegate double Function(double x);
+    class Delegate14
+    {
+        public static double FunctionValue(double x, Function f)
+        {
+            double res = 0;
+            for(int i = -1; i != 3; i += 4)
+            {
+                res += f(x);
+            }
+            return res;
+        }
+    }
+}

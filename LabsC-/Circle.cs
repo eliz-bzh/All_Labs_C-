a0a3e01253fda4bcbbe150268b0fbd6badf975b5
Liz <eliz.com>
2020-03-13 16:55:21 +0300

    13.1.1

---
+++ LabsC-/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabsC_
+{
+    class Circle
+    {
+        private double r;
+
+        public double R { get => r;
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new Exception("Radius can't be negative or equal 0");
+                }
+                r = value;
+            }
+        }
+
+        public Circle() : this(1) { }
+
+        public Circle(double r)
+        {
+            this.R = r;
+        }
+
+        public double Circumference { get => 2 * Math.PI * R; }
+        public double AreaOfTheCircle { get => Math.PI * Math.Pow(R, 2); }
+    }
+}

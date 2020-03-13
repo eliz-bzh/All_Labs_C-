69abf22e8758b7e34d818d721f7c893b49d1f88d
Liz <eliz.com>
2020-03-13 17:01:35 +0300

    12.2

---
+++ LabsC-/MailAdressComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabsC_
+{
+    class MailAdressComparer : IComparer<MailAdress>
+    {
+        public int Compare(MailAdress x, MailAdress y)
+        {
+            return x.House - y.House;
+        }
+    }
+}
